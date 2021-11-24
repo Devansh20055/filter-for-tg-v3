6e31d4a00b6cce3a52ef6aa66cc3d53406f269f5
Selfie SD <webtamil0@gmail.com>
2021-11-24 22:11:47 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -13,7 +13,7 @@ from utils import get_size, is_subscribed, temp
 import re
 logger = logging.getLogger(__name__)
 
-@Client.on_message(filters.command("start"))
+@Client.on_message(filters.command("flim"))
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
