85a7b348b0d7b4741f7c65b94788d5cfbd2ac252
Selfie SD <webtamil0@gmail.com>
2021-11-24 22:04:43 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -17,11 +17,13 @@ logger = logging.getLogger(__name__)
 async def start(client, message):
     if message.chat.type in ['group', 'supergroup']:
         buttons = [
-InlineKeyboardButton('🔍Search🔎', switch_inline_query_current_chat='')
-],[
-InlineKeyboardButton('⚠️ Help ⚠️', callback_data='help'),
-InlineKeyboardButton('🤓 About 🤓', callback_data='about')
-]]
+[
+InlineKeyboardButton('Search', url='https://t.me/TeamEvamaria')
+],
+[
+InlineKeyboardButton('Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
+]
+]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply(script.START_TXT.format(message.from_user.mention if message.from_user else message.chat.title, temp.U_NAME, temp.B_NAME), reply_markup=reply_markup)
         await asyncio.sleep(2) # 😢 https://github.com/EvamariaTG/EvaMaria/blob/master/plugins/p_ttishow.py#L17 😬 wait a bit, before checking.
