49d4deadcce5f4c153398121f07a67a7e0f936ca
Tamilnaducm <92597966+Tamilnaducm@users.noreply.github.com>
2021-12-14 09:22:51 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -35,8 +35,9 @@ InlineKeyboardButton('ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat='')
     if len(message.command) != 2:
         buttons = [[InlineKeyboardButton('ᴀᴅᴅ ᴍᴏᴠɪᴇ ꜱᴇᴀʀᴄʜ ʙᴏᴛ ᴛᴏ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
 ],[InlineKeyboardButton('ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat='')
-],[InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help')
-],[InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
+],[
+InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
+InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
@@ -72,8 +73,9 @@ InlineKeyboardButton('ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat='')
     if len(message.command) ==2 and message.command[1] in ["subscribe", "error", "okay", "help"]:
         buttons = [[InlineKeyboardButton('ᴀᴅᴅ ᴍᴏᴠɪᴇ ꜱᴇᴀʀᴄʜ ʙᴏᴛ ᴛᴏ ɢʀᴏᴜᴘ', url=f'http://t.me/{temp.U_NAME}?startgroup=true')
 ],[InlineKeyboardButton('ꜱᴇᴀʀᴄʜ', switch_inline_query_current_chat='')
-],[InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help')
-],[InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
+],[
+InlineKeyboardButton('ʜᴇʟᴘ', callback_data='help'),
+InlineKeyboardButton('ᴀʙᴏᴜᴛ', callback_data='about')
 ]]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_photo(
