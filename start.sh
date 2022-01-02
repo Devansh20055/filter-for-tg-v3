c1a9593d5a3c6edcc71709aef647c4c86703dc30
me is noob <79394737+Devansh20055@users.noreply.github.com>
2022-01-02 21:22:06 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git https://github.com/Devansh20055/filter-for-tg-v3.git /filter-for-tg-v3
+  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /EvaMaria
