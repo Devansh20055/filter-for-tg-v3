ac845ea19b5ff88933d96bad0c2499469f76be40
me is noob <79394737+Devansh20055@users.noreply.github.com>
2022-01-02 21:02:02 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,9 +4,9 @@ then
   git https://github.com/Devansh20055/filter-for-tg-v3.git /filter-for-tg-v3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /filter-for-tg-v3
+  git clone $UPSTREAM_REPO /EvaMaria
 fi
-cd /filter-for-tg-v3
+cd /EvaMaria
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
