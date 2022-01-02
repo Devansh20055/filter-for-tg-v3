1082394369c31da142ea6c50c014c6b5fda76b5b
me is noob <79394737+Devansh20055@users.noreply.github.com>
2022-01-02 20:55:42 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git https://github.com/Devansh20055/filter-for-tg-v3.git /filter-for-tg-v3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /filter-for-tg-v3
 fi
-cd /EvaMaria
+cd /filter-for-tg-v3
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
