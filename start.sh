9e2e763f2800831687db315678669fca78a4e4ff
me is noob <79394737+Devansh20055@users.noreply.github.com>
2022-01-02 21:35:28 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/Devansh20055/filter-for-tg-v3.git /filter-for-tg-v3
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /filter-for-tg-v3
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
