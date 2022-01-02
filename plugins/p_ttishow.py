a88b834c0621fd605cff9f3e8006b94b0545d2d1
me is noob <79394737+Devansh20055@users.noreply.github.com>
2022-01-03 03:47:24 +0530

    Update p_ttishow.py

--- plugins/p_ttishow.py
+++ plugins/p_ttishow.py
@@ -38,7 +38,7 @@ async def save_group(bot, message):
             return
         buttons = [[
             InlineKeyboardButton('ℹ️ Help', url=f"https://t.me/{temp.U_NAME}?start=help"),
-            InlineKeyboardButton('📢 Updates', url='https://t.me/groupdcbots')
+            InlineKeyboardButton('📢 Updates', url='https://t.me/off_chats')
         ]]
         reply_markup=InlineKeyboardMarkup(buttons)
         await message.reply_text(
